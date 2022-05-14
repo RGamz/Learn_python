d5ff489a88a538b23d82aa629068e887fb934ce8
Gamzat <rgamzat@live.com>
2022-05-14 19:30:48 +0300

    Остановился на опрелении функции Get

--- main.py
+++ main.py
@@ -1,9 +1,47 @@
-def print_args(**kwargs):
-    print(kwargs)
+namespaces = {}
+
+def Create():
+    """
+    Create <namespace> <parent>
+    создать новое пространство имен с именем <namespace> внутри пространства <parent>
+    """
+    local_namespace = {}
+    local_namespace[namespace] = []
+    namespaces[arg] = local_namespace
+
+def Add():
+    """
+    add <namespace> <var>
+    добавить в пространство <namespace> переменную <var>
+    """
+    if namespace in namespaces:
+        namespaces[namespace] = arg
+    else:
+        return "Error"
+
+def Get():
+    """
+    get <namespace> <var>
+    получить имя пространства, из которого будет взята переменная <var>
+    при запросе из пространства <namespace>, или None, если такого пространства не существует
+    """
+    pass
+
+for i in range(int(input())):
+    cmd, namespace, arg = input().split()
+    if cmd == "create":
+        Create()
+    elif cmd == "add":
+        Add()
+    elif cmd == "get":
+        Get()
 
 
-print_args(x = "one", y = "two", z = "three")
+print(namespaces)
 
-print('one')
+
+
+
+
 
 
