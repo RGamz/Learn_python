6db44bbb25342aa9417496e8489c93a2c47fa15d
Gamzat <rgamzat@live.com>
2022-05-15 13:18:28 +0300

    Получил корректный ответ от функции Create

--- main.py
+++ main.py
@@ -1,47 +1,77 @@
-namespaces = {}
+namespaces = {'global': None}
+variables = {}
 
-def Create():
+def Create(x):
     """
     Create <namespace> <parent>
     создать новое пространство имен с именем <namespace> внутри пространства <parent>
+    Примеры:
+        create foo global
+        create bar foo
     """
-    local_namespace = {}
-    local_namespace[namespace] = []
-    namespaces[arg] = local_namespace
+    if cmd in namespaces:
+        pass
+    else:
+        namespaces[namespace] = arg
 
-def Add():
+def Add(x):
     """
     add <namespace> <var>
     добавить в пространство <namespace> переменную <var>
+    Примеры:
+        add global a
+        add foo b
+        add bar a
     """
-    if namespace in namespaces:
-        namespaces[namespace] = arg
-    else:
-        return "Error"
+    variables[namespace] = []
+    a = variables[namespace]
+    a.append(arg)
 
-def Get():
+
+def Get(x):
     """
     get <namespace> <var>
     получить имя пространства, из которого будет взята переменная <var>
     при запросе из пространства <namespace>, или None, если такого пространства не существует
+    Примеры:
+        get foo a
+        get foo с
+        get bar a
+        get bar b
     """
-    pass
+    for key, value in namespaces.items():
+        if value == arg:
+            print("ok")
+        else:
+            return None
 
 for i in range(int(input())):
     cmd, namespace, arg = input().split()
     if cmd == "create":
-        Create()
+        Create(cmd)
     elif cmd == "add":
-        Add()
+        Add(cmd)
     elif cmd == "get":
-        Get()
+        Get(cmd)
 
 
 print(namespaces)
+print(variables)
 
+"""
 
+9
+add global a
+create foo global
+add foo b
+get foo a
+get foo c
+create bar foo
+add bar a
+get bar a
+get bar b
 
-
+"""
 
 
 
