51b54d031281aef8b17ad9317e863397b56649f0
Gamzat <rgamzat@live.com>
2022-07-29 11:35:06 +0300

    Получил корректный ответ от 3х внешних функций с проверкой на выполнение их условий если pos >= neg Получил корректный ответ от 1й внешней функции с проверкой на выполнение ее условия. Получил корректный ответ от 1й внешней функии. Получил корректный ответ от вывода вводимого списка.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 
 class multifilter:
 
-    def __init__(self, iterable, func):
+    def __init__(self, iterable, *funcs):
         self.iterable = iterable
         self.counter = 0
-        self.func = func
+        self.funcs = funcs
+        self.pos = 0
+        self.neg = 0
 
     def __iter__(self):
         return self
@@ -12,10 +14,20 @@ class multifilter:
     def __next__(self):
         if self.counter < len(self.iterable):
             self.counter += 1
-            if self.func(self.iterable[self.counter - 1]) is True:
+            for func in self.funcs:
+                if func(self.iterable[self.counter - 1]) is True:
+                    self.pos += 1
+                else:
+                    self.neg += 1
+            if self.pos >= self.neg:
+                self.pos = 0
+                self.neg = 0
                 return self.iterable[self.counter - 1]
             else:
+                self.pos = 0
+                self.neg = 0
                 return self.__next__()
+
         else:
             raise StopIteration
 
@@ -24,8 +36,15 @@ def mul2(x):
     return x % 2 == 0
 
 
+def mul3(x):
+    return x % 3 == 0
+
+
+def mul5(x):
+    return x % 5 == 0
+
 
 a = [i for i in range(31)]
 
-print(list(multifilter(a, mul2)))
+print(list(multifilter(a, mul2, mul3, mul5)))
 
