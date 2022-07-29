c9fb18fa61646a5ee841526f122f64e5d7e5d140
Gamzat <rgamzat@live.com>
2022-07-29 10:35:38 +0300

    Получил корректный ответ от 1й внешней функции с проверкой на выполнение ее условия. Получил корректный ответ от 1й внешней функии. Получил корректный ответ от вывода вводимого списка.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 class multifilter:
 
     def __init__(self, iterable, func):
@@ -11,13 +12,17 @@ class multifilter:
     def __next__(self):
         if self.counter < len(self.iterable):
             self.counter += 1
-            return self.func(self.iterable[self.counter - 1])
+            if self.func(self.iterable[self.counter - 1]) is True:
+                return self.iterable[self.counter - 1]
+            else:
+                return self.__next__()
         else:
             raise StopIteration
 
 
 def mul2(x):
-    return x * 20
+    return x % 2 == 0
+
 
 
 a = [i for i in range(31)]
