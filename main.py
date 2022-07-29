09c085ab0dc48c81919a906f9277991800893b34
Gamzat <rgamzat@live.com>
2022-07-29 10:19:17 +0300

    Получил корректный ответ от вывода вводимого списка.

--- main.py
+++ main.py
@@ -1,108 +1,77 @@
-namespaces = {'global': "None"}
-variables = {'global': []}
+# class multifilter:
+#     def judge_half(pos, neg):
+#         if pos >= neg:
+#             return True
+#         else:
+#             return False
+#         # допускает элемент, если его допускает хотя бы половина фукнций (pos >= neg)
+#
+#     def judge_any(pos, neg):
+#         if pos >= 1:
+#             return True
+#         else:
+#             return False
+#         # допускает элемент, если его допускает хотя бы одна функция (pos >= 1)
+#
+#     def judge_all(pos, neg):
+#         if neg == 0:
+#             return True
+#         else:
+#             return False
+#         # допускает элемент, если его допускают все функции (neg == 0)
+#
+#     def __init__(self, iterable, *funcs, judge=judge_all):
+#         self.iterable = iterable
+#         self.funcs = funcs
+#         self.judge = judge
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         pass
+#
+#
+#
+# def mul2(x):
+#     return x % 2 == 0
+#
+# def mul3(x):
+#     return x % 3 == 0
+#
+# def mul5(x):
+#     return x % 5 == 0
+#
+# a = [i for i in range(31)]  # [0, 1, 2, ... , 30]
+#
+# print(list(multifilter(a, mul2, mul3, mul5)))
+# # [0, 2, 3, 4, 5, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30]
+#
+# print(list(multifilter(a, mul2, mul3, mul5, judge=multifilter.judge_half)))
+# # [0, 6, 10, 12, 15, 18, 20, 24, 30]
+#
+# print(list(multifilter(a, mul2, mul3, mul5, judge=multifilter.judge_all)))
+# # [0, 30]
 
-def Create(namespace):
-    """
-    Create <namespace> <parent>
-    создать новое пространство имен с именем <namespace> внутри пространства <parent>
-    Примеры:
-        create foo global
-        create bar foo
-    """
-    if namespace in namespaces:
-        pass
-    else:
-        namespaces[namespace] = arg
 
-def Add(namespace, arg):
-    """
-    add <namespace> <var>
-    добавить в пространство <namespace> переменную <var>
-    Примеры:
-        add global a
-        add foo b
-        add bar a
-    """
-    if namespace in variables.keys():
-        pass
-    else:
-        variables[namespace] = []
+class multifilter:
 
-    a = variables[namespace]
-    a.append(arg)
+    def __init__(self, iterable):
+        self.iterable = iterable
+        self.counter = 0
 
+    def __iter__(self):
+        return self
 
-def get(namespace, arg):
-
-    def check_fun(namespace):
-        for key in namespaces.keys():
-            if namespace == key:
-                return "True"
-            else:
-                continue
-
-
-    def check_var(arg):
-        for key, value in variables.items():
-            for x in value:
-                if arg == x:
-                    return "True"
-                else:
-                    continue
-            else:
-                continue
-
-    def get_var(namespace, arg):
-        for key, value in variables.items():
-            for x in value:
-                if arg == x:
-                    if key == namespace:
-                        return key
-                    else:
-                        continue
-                else:
-                    continue
-
-    if check_var(arg) is None:
-        return None
-    elif check_fun(namespace) is None:
-        return None
-    else:
-        if get_var(namespace, arg) is None:
-            namespace = namespaces[namespace]
-            if namespace == "None":
-                return "None"
-            else:
-                return get(namespace, arg)
+    def __next__(self):
+        if self.counter < len(self.iterable):
+            self.counter += 1
+            return self.iterable[self.counter - 1]
         else:
-            return get_var(namespace, arg)
+            raise StopIteration
 
 
-for i in range(int(input())):
-    cmd, namespace, arg = input().split()
-    if cmd == "create":
-        Create(namespace)
-    elif cmd == "add":
-        Add(namespace, arg)
-    elif cmd == "get":
-        print(get(namespace, arg))
+a = [1, 2, 3, 4]
 
+print(list(multifilter(a)))
 
-print(namespaces)
-print(variables)
-
-
-"""
-
-9
-add global a
-create foo global
-add foo b
-get foo a
-get foo c
-create bar foo
-add bar a
-get bar a
-get bar b
-
-"""
